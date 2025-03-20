30275765a535567baae5eb57d77233da162416d5
Weich, Luis <luis.weich@study.thws.de>
2025-03-20 20:39:28 +0100

    update

--- src/main/java/de/thws/powerplants/infrastructure/PowerplantsServer.java
+++ src/main/java/de/thws/powerplants/infrastructure/PowerplantsServer.java
@@ -4,4 +4,7 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class PowerplantsServer extends Application {}
+public class PowerplantsServer extends Application {
+
+    public PowerplantsServer() {}
+}
